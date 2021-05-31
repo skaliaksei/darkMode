b0c455316e2c3b6390ee88a27505b7c29497f4e8
Aliaksei Skakun <skaliaksei@gmail.com>
2021-05-31 22:19:42 +0300

    feat: develop a new switch function

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const btn = document.querySelector('#checker');
 const darkClass = document.querySelector('body');
 let localData = localStorage.getItem('theme')
 
+const switchKey = document.querySelector('.switch');
+const key = document.querySelector('.key');
+
 if(!localData) {
   localStorage.setItem('theme', 'light')
   darkClass.classList.add('light')
@@ -13,18 +16,45 @@ function setDark() {
   localStorage.setItem('theme', 'dark')
   darkClass.classList.add('dark')
   darkClass.classList.remove('light')
-  btn.innerText = 'Set Light'
+
+  key.classList.remove('swipe-left')
+  key.classList.add('swipe-right')
 }
 
 function setLight() {
   localStorage.setItem('theme', 'light')
   darkClass.classList.add('light')
   darkClass.classList.remove('dark')
-  btn.innerText = 'Set Dark'
+
+  key.classList.remove('swipe-right')
+  key.classList.add('swipe-left')
 }
 
-btn.onclick = function() {
+// btn.onclick = function() {
+//   if(localStorage.getItem('theme') === 'dark') {
+//     setLight();
+//   } else setDark();
+// }
+
+//-------------
+// Second example
+
+
+
+
+switchKey.onclick = function() {
   if(localStorage.getItem('theme') === 'dark') {
     setLight();
   } else setDark();
+
+
+  // if (count % 2 == 0) {
+  //   key.classList.remove('swipe-left')
+  //   key.classList.add('swipe-right')
+  // } else {
+  //   key.classList.remove('swipe-right')
+  //   key.classList.add('swipe-left')
+  // }
+
+
 }
